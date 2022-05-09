941bfc151b7f54faab4f95c5e21d723c354fcb5c
Nikita <lukashevich-nikitka@bk.ru>
2022-05-10 00:31:59 +0300

    feat: add saving languages add local storage. Submit

--- src/app.js
+++ src/app.js
@@ -351,7 +351,66 @@ allKeys.forEach((el) => el.addEventListener('mouseup', offKey));
 
 allKeys.forEach((el) => el.addEventListener('mousedown', pushKey));
 // ------------------------------------
+const firstRowRussianLettersNames = ['Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З'];
+const secondRowRussianLettersNames = ['Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д'];
+const thirdRowRussianLettersNames = ['Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б'];
+let index = 0;
 
+function languageInstall() {
+  if (index === 0) {
+    for (let i = 0; i < firstRowKeys.length; i += 1) {
+      firstRowKeys[i].innerText = firstRowKeysNames[i];
+      mainFirstRowKeys[3].classList.add('backspace');
+      mainFirstRowKeys[1].classList.add('minus');
+      mainFirstRowKeys[2].classList.add('plus');
+    }
+    for (let i = 0; i < secondRowKeys.length; i += 1) {
+      secondRowKeys[i].innerText = secondRowKeysNames[i];
+      mainSecondRowKeys[0].classList.add('tab');
+      mainSecondRowKeys[4].classList.add('del');
+      mainSecondRowKeys[3].classList.add('open-slash');
+      mainSecondRowKeys[2].classList.add('close-bracket');
+      mainSecondRowKeys[1].classList.add('open-bracket');
+    }
+    for (let i = 0; i < thirdRowKeys.length; i += 1) {
+      thirdRowKeys[i].className = 'keys';
+      row3.appendChild(thirdRowKeys[i]);
+      thirdRowKeys[i].innerText = thirdRowKeysNames[i];
+      mainThirdRowKeys[3].classList.add('enter');
+      mainThirdRowKeys[0].classList.add('capslock');
+      mainThirdRowKeys[1].classList.add('semicolon');
+      mainThirdRowKeys[2].classList.add('tick');
+    }
+    for (let i = 0; i < fourthRowKeys.length; i += 1) {
+      fourthRowKeys[i].innerText = fourthRowKeysNames[i];
+      mainFourthRowKeys[0].classList.add('left-shift');
+      mainFourthRowKeys[mainFourthRowKeys.length - 1].classList.add('right-shift');
+      mainFourthRowKeys[1].classList.add('open-slash-two');
+    }
+    for (let i = 0; i < fivethRowKeys.length; i += 1) {
+      fivethRowKeys[i].className = 'keys';
+      row5.appendChild(fivethRowKeys[i]);
+      fivethRowKeys[i].innerText = fivethRowKeysNames[i];
+      fivethRowKeys[0].classList.add('left-ctrl');
+      fivethRowKeys[5].classList.add('right-ctrl');
+      fivethRowKeys[3].classList.add('space');
+    }
+  } else {
+    for (let i = 0; i < secondRowKeys.length; i += 1) {
+      secondRowKeys[i + 5].innerText = firstRowRussianLettersNames[i];
+      secondRowKeys[1].innerText = 'X';
+      secondRowKeys[2].innerText = 'Ъ';
+      thirdRowKeys[i + 4].innerText = secondRowRussianLettersNames[i];
+      thirdRowKeys[1].innerText = 'Ж';
+      thirdRowKeys[2].innerText = 'Э';
+      fourthRowKeys[i].innerText = thirdRowRussianLettersNames[i];
+      fourthRowKeys[7].innerText = 'Shift';
+      fourthRowKeys[8].innerText = 'Я';
+    }
+  }
+}
+
+// ---------------
 let textAreaArray = [];
 
 let position = 0;
@@ -425,9 +484,6 @@ function cursorPrinter(e) {
     position = textArea.selectionEnd - (idx + 1);
     textArea.selectionStart = position;
     textArea.selectionEnd = position;
-    console.log(textArea.selectionStart);
-    console.log(textArea.selectionEnd);
-    console.log(position);
   } else if (e.target.textContent === '↓') {
     const idx = textAreaArray.indexOf('\n');
     position = textArea.selectionStart + (idx + 1);
@@ -444,14 +500,9 @@ allKeys.forEach((el) => el.addEventListener('mouseup', () => {
 
 // ------------------------------------------
 
-const firstRowRussianLettersNames = ['Й', 'Ц', 'У', 'К', 'Е', 'Н', 'Г', 'Ш', 'Щ', 'З'];
-const secondRowRussianLettersNames = ['Ф', 'Ы', 'В', 'А', 'П', 'Р', 'О', 'Л', 'Д'];
-const thirdRowRussianLettersNames = ['Ч', 'С', 'М', 'И', 'Т', 'Ь', 'Б'];
-
 const pressedSet = new Set();
 const codes = ['ShiftLeft', 'ControlLeft'];
 const letter = [']', 'Ъ'];
-let index = 0;
 
 document.addEventListener('keydown', (e) => {
   pressedSet.add(e.code);
@@ -488,16 +539,17 @@ document.addEventListener('keyup', () => {
 
 // ----------------local Storage-------------
 
-// function setLocalStorage() {
-//   localStorage.setItem('language', index);
-// }
+function setLocalStorage() {
+  localStorage.setItem('language', index);
+}
 
-// window.addEventListener('beforeunload', setLocalStorage);
+window.addEventListener('beforeunload', setLocalStorage);
 
-// function getLocalStorage() {
-//   if (localStorage.getItem('language')) {
-//     index = +localStorage.getItem('language');
-//   }
-// }
+function getLocalStorage() {
+  if (localStorage.getItem('language')) {
+    index = +localStorage.getItem('language');
+    languageInstall();
+  }
+}
 
-// window.addEventListener('load', getLocalStorage);
+window.addEventListener('load', getLocalStorage);
